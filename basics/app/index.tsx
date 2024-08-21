846c363470b92e0139b3ecadc7ad16c194a936fd
abdul <abdul.tanveer@gmail.com>
2024-08-21 11:17:43 +0530

    first commit

---
+++ basics/app/index.tsx
@@ -0,0 +1,33 @@
+import { Text, View ,StyleSheet} from "react-native";
+import React from 'react';
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={styles.bigBlue}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+
+ 
+}
+
